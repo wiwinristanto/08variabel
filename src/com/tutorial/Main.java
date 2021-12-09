950b0f855e01f0750fda98940e6f14d42eafd805
Wiwin Ristanto <wiwin.ristanto@STHIRA.LOCAL>
2021-12-09 16:25:30 +0700

    first commit

---
+++ src/com/tutorial/Main.java
@@ -0,0 +1,16 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args){
+        //membuat variabel
+        //type data
+        int x = 20; //assignment
+
+        System.out.println(x);
+
+        //kita membuat sebuah deklarasi
+        int b; //deklarasi
+        b = 7;
+        System.out.println(b);
+    }
+}
